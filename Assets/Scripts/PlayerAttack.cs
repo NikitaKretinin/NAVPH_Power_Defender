20b5260d4bcd97d0060e1735333eca013b1615f2
Viktor Modroczký <v.modroczky@gmail.com>
2023-12-10 00:18:38 +0100

    Fix attack

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,8 @@ public class PlayerAttack : MonoBehaviour
 {
     private int damageAmount; // Adjust the damage amount as needed.
     [SerializeField] GameObject player; // related player
+    bool triggered = false;
+    Damageable enemyDamageable = null;
 
     void Start()
     {
@@ -13,21 +15,28 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         damageAmount = player.GetComponent<Damageable>().getDamage();
-    }
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        // Check if the collision is with an object tagged as "Enemy."
-        if (collision.gameObject.CompareTag("EnemyAttackCollider"))
+        var attackPressed = Input.GetButtonDown("attack");
+
+        if (attackPressed && triggered)
         {
-            // Get the Damageable script component from the collided enemy.
-            Damageable enemyDamageable = collision.gameObject.transform.parent.GetComponent<Damageable>();
             Debug.Log(enemyDamageable);
             // If the enemy has a Damageable component, apply damage to it.
             if (enemyDamageable != null)
             {
                 enemyDamageable.TakeDamage(damageAmount);
             }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        // Check if the collision is with an object tagged as "Enemy."
+        triggered = collision.gameObject.CompareTag("EnemyAttackCollider");
+        if (triggered)
+        {
+            // Get the Damageable script component from the collided enemy.
+            enemyDamageable = collision.gameObject.transform.parent.GetComponent<Damageable>();
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,14 @@ public enum PlayerState
     interact
 }
 
+public enum PlayerDirection
+{
+    up,
+    down,
+    left,
+    right
+}
+
 public class PlayerMovement : MonoBehaviour
 {
     private bool isBuffActive = false;
@@ -17,6 +25,11 @@ public class PlayerMovement : MonoBehaviour
     public PlayerState currentState;
     private Rigidbody2D rb;
     bool isWalking = false;
+    [SerializeField] GameObject leftHitbox;
+    [SerializeField] GameObject rightHitbox;
+    [SerializeField] GameObject upHitbox;
+    [SerializeField] GameObject downHitbox;
+    PlayerDirection lastDirection = PlayerDirection.down;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +42,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (GetPlayerDirection())
+        {
+            case PlayerDirection.up:
+                upHitbox.SetActive(true);
+                downHitbox.SetActive(false);
+                leftHitbox.SetActive(false);
+                rightHitbox.SetActive(false);
+                break;
+            case PlayerDirection.down:
+                upHitbox.SetActive(false);
+                downHitbox.SetActive(true);
+                leftHitbox.SetActive(false);
+                rightHitbox.SetActive(false);
+                break;
+            case PlayerDirection.left:
+                upHitbox.SetActive(false);
+                downHitbox.SetActive(false);
+                leftHitbox.SetActive(true);
+                rightHitbox.SetActive(false);
+                break;
+            case PlayerDirection.right:
+                upHitbox.SetActive(false);
+                downHitbox.SetActive(false);
+                leftHitbox.SetActive(false);
+                rightHitbox.SetActive(true);
+                break;
+        }
+
         moveVelocity = Vector3.zero;
         moveVelocity.x = Input.GetAxisRaw("Horizontal");
         moveVelocity.y = Input.GetAxisRaw("Vertical");
@@ -97,5 +138,27 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(5.0f);
         isBuffActive = false;
         speed = prevSpeed;
+    }
+
+    PlayerDirection GetPlayerDirection()
+    {
+        if (moveVelocity.x > 0)
+        {
+            lastDirection = PlayerDirection.right;
+        }
+        else if (moveVelocity.x < 0)
+        {
+            lastDirection = PlayerDirection.left;
+        }
+        else if (moveVelocity.y > 0)
+        {
+            lastDirection = PlayerDirection.up;
+        }
+        else if (moveVelocity.y < 0)
+        {
+            lastDirection = PlayerDirection.down;
+        }
+
+        return lastDirection;
     }
 }
