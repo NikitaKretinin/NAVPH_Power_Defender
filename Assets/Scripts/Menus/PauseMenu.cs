13d3183dae4edaf3efef6db2e5710e0433d66fec
Viktor Modroczký <v.modroczky@gmail.com>
2023-12-13 12:49:00 +0100

    Add controls button to pause menu

--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -3,11 +3,13 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class PauseBehavior : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
   [SerializeField] GameObject pauseScreen;
   [SerializeField] GameObject resumeButton;
+  [SerializeField] GameObject controlsButton;
   [SerializeField] GameObject exitLevelButton;
+  [SerializeField] GameObject controlsScreen;
   [SerializeField] GameObject GUIElements;
   bool confirmedExit = false;
 
@@ -43,10 +45,24 @@ public class PauseBehavior : MonoBehaviour
     }
   }
 
+  void OpenControls()
+  {
+    pauseScreen.SetActive(false);
+    controlsScreen.SetActive(true);
+  }
+
+  void CloseControls()
+  {
+    pauseScreen.SetActive(true);
+    controlsScreen.SetActive(false);
+  }
+
   void Start()
   {
     gameObject.GetComponent<Button>().onClick.AddListener(Pause);
     resumeButton.GetComponent<Button>().onClick.AddListener(Resume);
+    controlsButton.GetComponent<Button>().onClick.AddListener(OpenControls);
+    controlsScreen.GetComponent<Button>().onClick.AddListener(CloseControls);
     exitLevelButton.GetComponent<Button>().onClick.AddListener(ExitLevel);
   }
 }
